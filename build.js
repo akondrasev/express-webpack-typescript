7ac379b539cc13f7ae0d9c789dc6a5282d2f3d87
Anton Kondrasev <anton.kondrasev@gmail.com>
2018-12-25 11:59:19 +0200

    manual build

---
+++ build.js
@@ -0,0 +1,18 @@
+const webpack = require("webpack");
+const devConfig = require("./webpack.dev.config");
+const prodConfig = require("./webpack.prod.config");
+const serverDevConfig = require("./webpack.server.dev.config");
+const serverProdConfig = require("./webpack.server.prod.config");
+const rimraf = require('rimraf');
+
+rimraf('/dist', function () {
+    console.log('dist removed');
+});
+
+const mode = process.env.mode || "production";
+
+const clientCompiler = webpack(mode === "production" ? prodConfig : devConfig);
+const serverCompiler = webpack(mode === "production" ? serverProdConfig : serverDevConfig);
+
+clientCompiler.run();
+serverCompiler.run();
