3419924e3f47d3882f0885e74fa43dc52945bcd6
Anton Kondrasev <anton.kondrasev@gmail.com>
2018-12-28 19:39:06 +0200

    socket typed, server webpack devtools

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,7 +3,9 @@ import './css/style.css';
 import io from 'socket.io-client';
 import Socket = SocketIOClient.Socket;
 
-const socket:Socket = io();
+const socket:Socket = io({
+    autoConnect: false
+});
 
 // Needed for Hot Module Replacement
 if (typeof (module.hot) !== 'undefined') {
