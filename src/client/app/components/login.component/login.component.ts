a322f9a3a56937571dd26d9ee2c725070eb8b835
Anton Kondrasev <anton.kondrasev@gmail.com>
2018-12-29 22:32:27 +0200

    ng messages

--- src/client/app/components/login.component/login.component.ts
+++ src/client/app/components/login.component/login.component.ts
@@ -1,11 +1,12 @@
 import * as angular from 'angular';
+import './login.component.scss';
 import {StateProvider} from "@uirouter/angularjs";
 
 const ngModule = angular.module("login", []).component("loginComponent", {
     templateUrl: './login.component.html',
-    controller: function () {
-        console.log("login component");
-    }
+    controller: [function () {
+
+    }]
 }).config(["$stateProvider", ($stateProvider:StateProvider) => {
     $stateProvider.state({
         name: 'login',
--- src/client/app/index.ts
+++ src/client/app/index.ts
@@ -1,17 +1,15 @@
 import * as angular from 'angular';
 import * as ngMaterial from 'angular-material';
+import 'angular-messages';
 import '@uirouter/angularjs';
 import {ICompileProvider} from "angular";
 import viewComponents from "./components/index";
 
-
-angular.module("app", [ngMaterial, "ui.router", viewComponents]).config(["$compileProvider", ($compileProvider: ICompileProvider) => {
+angular.module("app", [ngMaterial, "ui.router", viewComponents, "ngMessages"]).config(["$compileProvider", ($compileProvider: ICompileProvider) => {
     // @ts-ignore
     if (compile.isProduction) {
         $compileProvider.debugInfoEnabled(false);
     }
-}]).run([() => {
-
 }]);
 
 
