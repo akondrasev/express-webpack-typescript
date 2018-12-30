db8c10c11a107c239a5444675bb9adc2108965ce
Anton Kondrasev <anton.kondrasev@gmail.com>
2018-12-30 15:43:44 +0200

    redirect to login

--- src/client/app/components/login.component/login.component.ts
+++ src/client/app/components/login.component/login.component.ts
@@ -1,17 +1,39 @@
 import * as angular from 'angular';
 import './login.component.scss';
-import {StateProvider} from "@uirouter/angularjs";
+import {StateProvider, TransitionService, StateService} from "@uirouter/angularjs";
+import {IRootScopeService} from "angular";
 
 const ngModule = angular.module("login", []).component("loginComponent", {
     templateUrl: './login.component.html',
     controller: [function () {
 
     }]
-}).config(["$stateProvider", ($stateProvider:StateProvider) => {
+}).config(["$stateProvider", ($stateProvider: StateProvider) => {
     $stateProvider.state({
         name: 'login',
         url: '/login',
         component: 'loginComponent'
+    });
+}]).run(["$rootScope", "$state", ($rootScope: IRootScopeService, $state: StateService) => {
+    $state.go('login');
+
+    $rootScope.$on('$stateChangeStart', function (e, toState, toParams, fromState, fromParams) {
+
+        const isLogin = toState.name === "login";
+        if (isLogin) {
+            return;
+        }
+
+        $state.go('login');
+
+        // now, redirect only not authenticated
+
+        // var userInfo = authenticationSvc.getUserInfo();
+
+        // if(userInfo.authenticated === false) {
+        //     e.preventDefault(); // stop current execution
+        //     $state.go('login'); // go to login
+        // }
     });
 }]);
 
