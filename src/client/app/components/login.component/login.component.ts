1c76877ed4076ec8091a3934c56b1886189110cc
Anton Kondrasev <anton.kondrasev@gmail.com>
2019-01-12 13:35:43 +0200

    google login button

--- src/client/app/components/login.component/login.component.ts
+++ src/client/app/components/login.component/login.component.ts
@@ -13,6 +13,25 @@ class LoginComponent {
 
     }
 
+    $onInit() {
+        // @ts-ignore
+        gapi.signin2.render("google-id", {
+            "scope": "profile email openid",
+            // "width": 200,
+            // "height": 40,
+            "longtitle": false,
+            // "theme": "dark",
+            "onsuccess": function (googleUser) {
+                // Called when the user signs in
+                console.log(googleUser);
+            },
+            "onfailure": function (e) {
+                console.warn("Google Sign-In failure: " + e.error);
+            }
+        });
+    }
+
+
     login() {
         this.authService.login(this.email, this.password).then(() => {
             this.$state.go("chat")
